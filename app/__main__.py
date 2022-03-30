5f0feda54a31d3530c9a5cab6b9f3b60bb52e277
Vincent74230 <vincent.nowak@hotmail.fr>
2022-03-30 17:13:08 +0200

    RENAMED main into app, and added simple distance sensor test

--- app/__main__.py
+++ app/__main__.py
@@ -5,7 +5,9 @@ from config import WATER
 
 def main():
     """Main function of the app"""
+    print(tools.distance_sensor())
     tools.water_plants(WATER)
+    print(tools.distance_sensor())
 
 if __name__ == '__main__':
     main()
